d889f408f352d55fcac6f4be145144b45b6feb32
Bartlomiej Wojtowicz <b.wojtowicz@selleo.com>
2018-03-07 16:07:32 +0100

    Init from template

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib', require: false
+gem 'foreman'
+gem 'puma'
+gem 'rake'
+
+gem 'pg'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+group :development do
+  gem 'pry'
+  gem 'pry-remote'
+  gem 'rerun'
+end
+
+gem 'haml'
---
+++ app/boot.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require 'sinatra/base'
+require 'sinatra/activerecord'
+
+autoload_dirs = %w[
+  lib
+  models
+  helpers
+]
+
+autoload_dirs.each do |dir|
+  Dir[File.join(File.expand_path(File.dirname(__FILE__)), dir, '**', '*.rb')].each { |f| require f }
+end
+
